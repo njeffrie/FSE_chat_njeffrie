6561193310d6000617b7c28979eb3b4450271257
Nat Jeffries <natmjeffries@gmail.com>
2015-01-15 17:33:15 -0500

    inital commit for node.js version

---
+++ index.js
@@ -0,0 +1,23 @@
+/** @file index.js
+ *  @brief extremely simple Node.js server, inspiration from Node.js 
+ *  online tutorial
+ *  @author Nat Jeffries
+ */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
